d1089d96d044ab4bc3900a1b76fde51175d7be36
John Kim <johnjkim31@gmail.com>
2025-07-30 23:56:57 -0700

    finished training and gear sections

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { Text,Image, StyleSheet, Platform, View, TextInput, TouchableOpacity, Pr
 import Header from '../header';
 import React, {useState, useCallback, useEffect, useRef} from 'react';
 import Event from '../event'
+import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
   const events = [
@@ -23,6 +24,42 @@ const HomeScreen = () => {
       dateTime: 'Sun, 2:00 PM',
       location: 'Mennen Sports Arena'
     }
+  ];
+
+  const trainings = [
+    {
+      trainingType: 'Stickhandling',
+      trainingName: 'Master Your Dangles',
+      description: 'Improve puck control. 15 mins'
+    },
+    {
+      trainingType: 'Shooting',
+      trainingName: 'Sniper School',
+      description: 'Increase shot power. 20 mins.'
+    },
+    {
+      trainingType: 'Strength',
+      trainingName: 'Hockey Core Power',
+      description: 'Build stability. 15 mins.'
+    }
+  ];
+
+  const gear = [
+    {
+      title: 'Youth Sports Drive', 
+      type: 'Donation Center',
+      location: '123 Main St'
+    },
+    {
+      title: 'Hockey Pro Shop',
+      type: 'Retail Store',
+      location: '456 Rink Ave.'
+    },
+    {
+      title: 'Bauer Skates Size 8',
+      type: 'User Donation',
+      location: 'J. Doe'
+    }
   ]
 
   return (
@@ -31,12 +68,18 @@ const HomeScreen = () => {
       <ScrollView>
         <View style={styles.eventsContainer}>
           <Text style={styles.subheaderText}>Nearby Activities and Events</Text>
-          <View style={styles.eventListContainer}>
+          <View style={styles.cardListContainer}>
             {events.map((event) => (
-              <View key={event.activityName} style={styles.eventContainer}>
-                <Text>{event.activityName}</Text>
-                <Text>{event.dateTime}</Text>
-                <Text>{event.location}</Text>
+              <View key={event.activityName} style={styles.card}>
+                <Text style={styles.cardTitleText}>{event.activityName}</Text>
+                <View style={styles.calendarContainer}>
+                  <Ionicons name='calendar-outline' size={16}/>
+                  <Text style={styles.calendarText}>{event.dateTime}</Text>
+                </View>
+                <View style={styles.locationContainer}>
+                  <Ionicons name='location-sharp' size={16} />
+                  <Text style={styles.locationText}>{event.location}</Text>
+                </View>
                 <Pressable 
                   style={styles.geminiButton} 
                   onPress={() => console.log('Gemini output here')}
@@ -49,9 +92,44 @@ const HomeScreen = () => {
         </View>
         <View>
           <Text style={styles.subheaderText}>Train at Home</Text>
+          <View style={styles.cardListContainer}>
+            {trainings.map((training) => (
+              <View key={training.trainingName} style={styles.card}>
+                <Text style={styles.cardTitleText}>{training.trainingName}</Text>
+                <Text>{training.description}</Text>
+                <Text style={styles.label}>{training.trainingType}</Text>
+                <Pressable 
+                  style={styles.geminiButton}
+                  onPress={() => console.log('Gemini output here')}
+                >
+                  <Text style={styles.geminiButtonText}>Customize Drill</Text>
+                </Pressable>
+              </View>
+            ))}
+          </View>
         </View>
         <View>
-          <Text style={styles.subheaderText}>Find Your Gear</Text>
+          <View style={styles.gearHeadingContainer}>
+              <Text style={styles.subheaderText}>Find Your Gear</Text>
+              <Pressable 
+                style={styles.geminiButton}
+                onPress={() => console.log('Gemini output here')}
+              >
+                <Text style={styles.geminiButtonText}>What Gear Do I Need?</Text>
+              </Pressable>
+          </View>
+          <View style={styles.cardListContainer}>
+            {gear.map((gear) => (
+              <View key={gear.title} style={styles.card}>
+                <Text style={styles.cardTitleText}>{gear.title}</Text>
+                <View style={styles.locationContainer}>
+                  <Ionicons name='location-sharp' size={16} />
+                  <Text style={styles.locationText}>{gear.location}</Text>
+                </View>
+                <Text style={styles.label}>{gear.type}</Text>
+              </View>
+            ))}
+          </View>
         </View>
       </ScrollView>
       
@@ -63,15 +141,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  eventContainer: {
+  card: {
     backgroundColor: 'white',
     borderRadius: 8,
     padding: 16,
     margin: 8
   },
-  eventListContainer: {
+  cardListContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap'
+  },
+  cardTitleText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  label: {
+    alignSelf: 'flex-start',
+    padding: 8,
+    marginBottom: 8,
+    marginTop: 8,
+    backgroundColor: '#4b8bfa',
+    color: 'white',
+    borderRadius: 4
+  },
+  calendarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  calendarText: {
+    padding: 8
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  locationText: {
+    padding: 8
   },
   eventsContainer: {
 
@@ -92,11 +197,17 @@ const styles = StyleSheet.create({
   geminiButton: {
     backgroundColor: '#4b8bfa',
     padding: 8,
-    borderRadius: 8
+    borderRadius: 8,
+    justifyContent: 'center'
   },
   geminiButtonText: {
     color: 'white',
     textAlign: 'center'
+  },
+  gearHeadingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8
   }
   
 });
